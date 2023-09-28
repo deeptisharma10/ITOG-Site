8b6c33446213a99e5325fed0ae7c0247ab0fb186
Deepti Sharma <124440255+deeptisharma10@users.noreply.github.com>
2023-09-28 19:12:56 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+jQuery('.stic').stickMe({
+	topOffset: 300,
+	transitionDuration: 500,
+shadow: false,
+shadowOpacity: 0.3,
+animate: false,
+triggerAtCenter: true,
+transitionStyle: 'fade',
+stickyAlready: false,
+
+	
+	});
